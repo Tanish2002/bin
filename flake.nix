74d278b6af0b1317541a8aead31eb5d9e8031c8e
Tanish2002 <tanishkhare@gmail.com>
2022-03-02 02:14:45 +0530

    add a few more deps to flake.nix

--- flake.nix
+++ flake.nix
@@ -3,10 +3,26 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, ... }: {
 
-    defaultPackage.x86_64-linux =
-      with import nixpkgs { system = "x86_64-linux"; };
+    defaultPackage.x86_64-linux = with import nixpkgs {
+      system = "x86_64-linux";
+      overlays = [
+        (final: prev: {
+          colorpicker-ym1234 = prev.colorpicker.overrideAttrs (o: {
+            src = prev.fetchFromGitHub {
+              owner = "ym1234";
+              repo = "colorpicker";
+              rev = "3b5076f5bb3392830e7d1d97fa86621ca19c4d1a";
+              sha256 = "lt8l387Kc3RGCTEcE+zIf0sXdLrmVVu3L6qhhSlEQ2Q=";
+            };
+          });
+          # Imagemagick package is kinda broken :(
+          imagemagick-patch = prev.imagemagick.overrideAttrs
+            (o: { nativeBuildInputs = o.nativeBuildInputs ++ [ prev.curl ]; });
+        })
+      ];
+    };
       stdenv.mkDerivation {
         name = "scripts";
         src = ./scripts;
@@ -40,6 +56,10 @@
                   timg
                   rofi
                   jq
+                  file
+                  maim
+                  colorpicker-ym1234
+                  imagemagick-patch
                 ]
               }
           done
