e5f705c4002db61d809dd12de9ff78b389a4c167
Tanish2002 <tanishkhare@gmail.com>
2022-03-27 23:58:26 +0530

    testing

--- scripts/screenshot-menu.sh
+++ scripts/screenshot-menu.sh
@@ -2,7 +2,6 @@
 # Opens A rofi menu with screenshot Options
 
 chosen=$(printf "fullscreen\narea\narea [clipboard]\nactive window" | rofi -dmenu -lines 4 -width 25 -p "screenshot" -i)
-echo $HOME > /tmp/test
 echo $chosen
 if [ $WAYLAND_DISPLAY ]; then
 	case $chosen in
@@ -25,10 +24,10 @@ else
              sleep 0.5; maim ~/screenshots/$(date "+%F_%H:%M:%S").png && notify-send '	screenshot saved!' -t 1500 || break
              ;;
          "area")
-             maim -u -s ~/screenshots/$(date "+%F_%H:%M:%S").png && notify-send '	selected area screenshot saved!' -t 1500 || break
+             sleep 0.5; maim -u -s ~/screenshots/$(date "+%F_%H:%M:%S").png && notify-send '	selected area screenshot saved!' -t 1500 || break
              ;;
          "area [clipboard]")
-             maim -s | xclip -selection clipboard -t image/png && notify-send '	screenshot copied to clipboard' -t 1500 || break
+             sleep 0.5; maim -s | xclip -selection clipboard -t image/png && notify-send '	screenshot copied to clipboard' -t 1500 || break
              ;;
          "active window")
      	sleep 0.5; maim -u -i $(xdotool getactivewindow) ~/screenshots/$(date "+%F_%H:%M:%S").png && notify-send '	active window screenshot saved!' -t 1500 || break
