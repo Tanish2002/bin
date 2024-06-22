5122a37172dbe5fff1dea7bf517ff3d6426b39d1
Tanish2002 <tanishkhare@gmail.com>
2024-06-22 21:08:10 +0530

    small change to volume-control.sh

--- scripts/volume-control.sh
+++ scripts/volume-control.sh
@@ -5,8 +5,8 @@
 # --------------------------
 
 # Steps for raising/lowering volume
-STEP=5
-BIG_STEP=25
+STEP=${2:-5} # default 5%
+BIG_STEP=${2:25}
 
 # Notification config
 VOLUME_ICON=~/.config/awesome/themes/lovelace/icons/volume.png
@@ -14,37 +14,38 @@ MUTED_ICON=~/.config/awesome/themes/lovelace/icons/muted.png
 NOTIF_ID=/tmp/volume-daemon-notification
 
 if [[ "$1" = "up" ]]; then
-    pactl set-sink-mute @DEFAULT_SINK@ 0 && pactl set-sink-volume @DEFAULT_SINK@ +$STEP%
+	pactl set-sink-mute @DEFAULT_SINK@ 0 && pactl set-sink-volume @DEFAULT_SINK@ +$STEP%
 elif [[ "$1" = "UP" ]]; then
-    pactl set-sink-mute @DEFAULT_SINK@ 0 && pactl set-sink-volume @DEFAULT_SINK@ +$BIG_STEP%
+	pactl set-sink-mute @DEFAULT_SINK@ 0 && pactl set-sink-volume @DEFAULT_SINK@ +$BIG_STEP%
 elif [[ "$1" = "down" ]]; then
-    pactl set-sink-mute @DEFAULT_SINK@ 0 && pactl set-sink-volume @DEFAULT_SINK@ -$STEP%
+	pactl set-sink-mute @DEFAULT_SINK@ 0 && pactl set-sink-volume @DEFAULT_SINK@ -$STEP%
 elif [[ "$1" = "DOWN" ]]; then
-    pactl set-sink-mute @DEFAULT_SINK@ 0 && pactl set-sink-volume @DEFAULT_SINK@ -$BIG_STEP%
+	pactl set-sink-mute @DEFAULT_SINK@ 0 && pactl set-sink-volume @DEFAULT_SINK@ -$BIG_STEP%
 elif [[ "$1" = "toggle" ]]; then
-    pactl set-sink-mute @DEFAULT_SINK@ toggle
+	pactl set-sink-mute @DEFAULT_SINK@ toggle
 elif [[ "$1" = "reset" ]]; then
-    pactl set-sink-mute @DEFAULT_SINK@ 0 && pactl set-sink-volume @DEFAULT_SINK@ 50%
+	pactl set-sink-mute @DEFAULT_SINK@ 0 && pactl set-sink-volume @DEFAULT_SINK@ 50%
 else
-    echo "No argument."
+	echo "No argument."
 fi
 
 SIDEBAR_VISIBLE="$(awesome-client 'return sidebar.visible' | awk '{print $2}')"
 # echo $SIDEBAR_VISIBLE
 if [[ "$SIDEBAR_VISIBLE" == "false" ]]; then
-    var=$(pactl list sinks)
+	var=$(pactl list sinks)
 
-    case $var in
-        *'Mute: yes'*)
-            out="off"
-            ICON=$MUTED_ICON
-            ;;
-        *)
-            # we want word splitting
-            set -- ${var#*Volume:}
-            out="$a$t$4"
-            ICON=$VOLUME_ICON
-    esac
-    #echo "Volume: $out"
-    notify-send --urgency low -R "$NOTIF_ID" -i $ICON "$out"
+	case $var in
+	*'Mute: yes'*)
+		out="off"
+		ICON=$MUTED_ICON
+		;;
+	*)
+		# we want word splitting
+		set -- ${var#*Volume:}
+		out="$a$t$4"
+		ICON=$VOLUME_ICON
+		;;
+	esac
+	#echo "Volume: $out"
+	notify-send --urgency low -R "$NOTIF_ID" -i $ICON "$out"
 fi
